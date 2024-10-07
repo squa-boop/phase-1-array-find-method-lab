a519ef2bcd77177e092509680118a7566790c08d
blessed wesonga <blessedwesonga@gmail.com>
2024-10-07 23:03:24 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    
+  ];
+  
+  
+  console.log(superbowlWin(record)); 
